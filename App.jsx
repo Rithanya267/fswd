cb6ee2ef803d4b961fa47dcf79d762def8cf38cc
Rithanya S V <mrrithanya267@gmail.com>
2025-12-10 17:00:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import TicketBookingForm from "./Fullstackproject/TicketBooking";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <TicketBookingForm />
+    </div>
+  );
+}
+
+export default App;
